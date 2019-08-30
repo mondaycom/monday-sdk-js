446771a8e9d6221e3496490ba8912971cfa79b76
talharamati <tal@monday.com>
2019-08-31 00:21:55 +0300

    progress

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import mondayApi from "./monday-api-client";
 
 window.monday = {
+  listeners: {},
   init: client_id => {
     window.monday.client_id = client_id;
+    window.addEventListener("message", window.monday.receiveMessage, false);
   },
   token: token => {
     window.monday.token = token;
@@ -16,12 +18,29 @@ window.monday = {
       window.parent.postMessage({ method, args }, "*");
 
       var receiveMessage = event => {
-        resolve(event.data);
-        window.removeEventListener("message", receiveMessage);
+        if (event.data.method == method) {
+          resolve(event.data);
+          window.removeEventListener("message", receiveMessage);
+        }
       };
 
       window.addEventListener("message", receiveMessage, false);
     });
+  },
+  receiveMessage: (event) => {
+    const { method } = event.data;
+    const listeners = window.monday.listeners[method];
+    if (listeners) {
+      listeners.forEach(listener => {
+        listener(event.data);
+      });
+    }
+  },
+  listen: (type, callback) => {
+    window.monday.localApi("listen", { type });
+    window.monday.listeners[type] = window.monday.listeners[type] || [];
+    window.monday.listeners[type].push(callback);
+    // todo uniq listeners, remove listener
   },
   authenticate: () => {
     var url = `http://auth.lvh.me/oauth/authorize?client_id=${
