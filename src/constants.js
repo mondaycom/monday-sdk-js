f58124e1a610f124c84786ebafdd89c0f46826e0
Omer Doron <omer@monday.com>
2020-03-05 16:33:07 +0200

    constants unit tests

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,9 @@
 const { isBrowser } = require("./helpers");
 
-const MONDAY_PROTOCOL = (!isBrowser && process.env.MONDAY_COM_PROTOCOL) || "https";
-const MONDAY_DOMAIN = (!isBrowser && process.env.MONDAY_COM_DOMAIN) || "monday.com";
+const isNodeDevEnv = !isBrowser && process.env.NODE_ENV === "development";
+
+const MONDAY_PROTOCOL = (isNodeDevEnv && process.env.MONDAY_COM_PROTOCOL) || "https";
+const MONDAY_DOMAIN = (isNodeDevEnv && process.env.MONDAY_COM_DOMAIN) || "monday.com";
 const MONDAY_API_URL = `${MONDAY_PROTOCOL}://api.${MONDAY_DOMAIN}/v2`;
 const MONDAY_OAUTH_URL = `${MONDAY_PROTOCOL}://auth.${MONDAY_DOMAIN}/oauth2/authorize`;
 const MONDAY_OAUTH_TOKEN_URL = `${MONDAY_PROTOCOL}://auth.${MONDAY_DOMAIN}/oauth2/token`;
