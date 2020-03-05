09475e7d5c5c08ee1bfdbb1c33766a48431efa81
omerdoron1 <1omerdoron@gmail.com>
2020-03-05 12:52:27 +0200

    add linter and style check (#6)
    * add eslint
    
    * add prettier
    
    * add lint and style check

--- src/client.js
+++ src/client.js
@@ -38,9 +38,11 @@ class MondayClientSdk {
       return mondayApi(params, { token });
     } else {
       return new Promise((resolve, reject) => {
-        this._localApi("api", { params }).then(result => {
-          resolve(result.data);
-        });
+        this._localApi("api", { params })
+          .then(result => {
+            resolve(result.data);
+          })
+          .catch(err => reject(err));
       });
     }
   }
@@ -73,7 +75,7 @@ class MondayClientSdk {
   }
 
   _localApi(method, args) {
-    return new Promise((resolve, reject) => {
+    return new Promise(resolve => {
       const requestId = this._generateRequestId();
       const clientId = this._clientId;
 
@@ -90,11 +92,7 @@ class MondayClientSdk {
     const typeListeners = this.listeners[type] || EMPTY_ARRAY;
     const requestIdListeners = this.listeners[requestId] || EMPTY_ARRAY;
 
-    let listeners = [
-      ...methodListeners,
-      ...typeListeners,
-      ...requestIdListeners
-    ];
+    let listeners = [...methodListeners, ...typeListeners, ...requestIdListeners];
 
     if (listeners) {
       listeners.forEach(listener => {
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -2,8 +2,7 @@ const convertToArrayIfNeeded = x => {
   return Array.isArray(x) ? x : [x];
 };
 
-const isBrowser =
-  typeof window !== "undefined" && typeof window.document !== "undefined";
+const isBrowser = typeof window !== "undefined" && typeof window.document !== "undefined";
 
 module.exports = {
   convertToArrayIfNeeded,
