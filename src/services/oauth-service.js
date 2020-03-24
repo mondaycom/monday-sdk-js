f62c03930ed39938fb2e114096d42fd72dfccf10
Toot Moran <tootmoran@Toots-MacBook-Pro.local>
2020-03-24 18:43:38 +0200

    more

--- src/services/oauth-service.js
+++ src/services/oauth-service.js
@@ -3,7 +3,7 @@ const { MONDAY_OAUTH_TOKEN_URL } = require("../constants.js");
 
 const oauthToken = (code, clientId, clientSecret) => {
   const data = { code, client_id: clientId, client_secret: clientSecret };
-  return execute(data, { url: MONDAY_OAUTH_TOKEN_URL });
+  return execute(data, null, { url: MONDAY_OAUTH_TOKEN_URL });
 };
 
 module.exports = {
