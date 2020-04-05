828a158ddcaf862b938cd969c0b3696fea4723db
Omer Doron <omer@monday.com>
2020-04-05 09:27:29 +0300

    add a comment inside the css

--- src/helpers/ui-helpers.js
+++ src/helpers/ui-helpers.js
@@ -9,7 +9,7 @@ function initScrollHelperIfNeeded() {
 
   // when an elemnt cover the scollable element, wheel events are synchronous on chromium based browsers
   const css =
-    'body::before { content: ""; position: fixed; top: 0; right: 0; bottom: 0; left: 0; pointer-events: none; z-index: 2147483647; }';
+    'body::before { content: ""; position: fixed; top: 0; right: 0; bottom: 0; left: 0; pointer-events: none; z-index: 2147483647; /* mondaySdk css - can be disabled with: mondaySdk({withoutScrollHelper: true }) */ }';
   const style = document.createElement("style");
   style.appendChild(document.createTextNode(css));
 
