0be3c9ca1609decc2db5a6e2c60c52b26ae1ab80
Chris Packett <chris@packett.io>
2021-05-14 12:30:39 -0400

    ensures that this doesn't break any existing code

--- src/services/oauth-service.js
+++ src/services/oauth-service.js
@@ -2,7 +2,12 @@ const { execute } = require("../monday-api-client");
 const { MONDAY_OAUTH_TOKEN_URL } = require("../constants.js");
 
 const oauthToken = (code, clientId, clientSecret, redirectUri) => {
-  const data = { code, client_id: clientId, client_secret: clientSecret, redirect_uri: redirectUri };
+  let data = { code, client_id: clientId, client_secret: clientSecret };
+  
+  if (redirectUri) {
+    data.redirect_uri = redirectUri;
+  }
+  
   return execute(data, null, { url: MONDAY_OAUTH_TOKEN_URL });
 };
 
