19a21e0a3c588d13edac58a41ea8db1051a8aca1
Chris Packett <chris@packett.io>
2021-05-14 12:27:19 -0400

    Adds redirect_uri to oauth token request

--- src/services/oauth-service.js
+++ src/services/oauth-service.js
@@ -1,8 +1,8 @@
 const { execute } = require("../monday-api-client");
 const { MONDAY_OAUTH_TOKEN_URL } = require("../constants.js");
 
-const oauthToken = (code, clientId, clientSecret) => {
-  const data = { code, client_id: clientId, client_secret: clientSecret };
+const oauthToken = (code, clientId, clientSecret, redirectUri) => {
+  const data = { code, client_id: clientId, client_secret: clientSecret, redirect_uri: redirectUri };
   return execute(data, null, { url: MONDAY_OAUTH_TOKEN_URL });
 };
 
