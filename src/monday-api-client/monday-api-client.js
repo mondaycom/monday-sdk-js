a30012a075e0e8eafaa383feb4c1c0e0a84a1af3
Edo Garavani <edo@monday.com>
2021-07-26 18:54:04 +0300

    return appropriate error message when getting a timeout from the API

--- src/monday-api-client/monday-api-client.js
+++ src/monday-api-client/monday-api-client.js
@@ -3,6 +3,7 @@ const fetch = require("./fetch");
 
 const COULD_NOT_PARSE_JSON_RESPONSE_ERROR = "Could not parse JSON from monday.com's GraphQL API response";
 const TOKEN_IS_REQUIRED_ERROR = "Token is required";
+const API_TIMEOUT_ERROR = "Received timeout from monday.com's GraphQL API";
 
 function apiRequest(url, data, token, options = {}) {
   return fetch.nodeFetch(url, {
@@ -10,8 +11,8 @@ function apiRequest(url, data, token, options = {}) {
     body: JSON.stringify(data || {}),
     headers: {
       Authorization: token,
-      "Content-Type": "application/json"
-    }
+      "Content-Type": "application/json",
+    },
   });
 }
 
@@ -22,6 +23,17 @@ async function execute(data, token, options = {}) {
   const path = options.path || "";
   const fullUrl = `${url}${path}`;
   let response = await apiRequest(fullUrl, data, token, options);
+
+  const responseStatusCode = response.status;
+  const responseContentType = response.headers.get("content-type");
+  if (!responseContentType || !responseContentType.includes("application/json")) {
+    if (responseStatusCode === 504) {
+      throw new Error(API_TIMEOUT_ERROR);
+    }
+
+    const responseText = await response.text();
+    throw new Error(responseText);
+  }
 
   try {
     return await response.json();
