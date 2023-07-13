0ede6c19e5f9006b0ddf02267659f533e7cbbd49
Yoni Levin <parahall@gmail.com>
2023-07-13 15:18:00 +0300

    Fix

--- src/constants.js
+++ src/constants.js
@@ -4,9 +4,9 @@ const isNodeDevEnv = !isBrowser && process.env.NODE_ENV === "development";
 
 const MONDAY_PROTOCOL = (isNodeDevEnv && process.env.MONDAY_COM_PROTOCOL) || "https";
 const MONDAY_DOMAIN = (isNodeDevEnv && process.env.MONDAY_COM_DOMAIN) || "monday.com";
-const MONDAY_SUBDOMAIN_API = "api.";
+const MONDAY_SUBDOMAIN_API = (isNodeDevEnv && process.env.MONDAY_SUBDOMAIN_API) || "api.";
 const MONDAY_API_URL = `${MONDAY_PROTOCOL}://${MONDAY_SUBDOMAIN_API}${MONDAY_DOMAIN}/v2`;
-const MONDAY_OAUTH_SUBDOMAIN = `auth.`;
+const MONDAY_OAUTH_SUBDOMAIN = (isNodeDevEnv && process.env.MONDAY_OAUTH_SUBDOMAIN) || "auth.";
 const MONDAY_OAUTH_URL = `${MONDAY_PROTOCOL}://${MONDAY_OAUTH_SUBDOMAIN}${MONDAY_DOMAIN}/oauth2/authorize`;
 const MONDAY_OAUTH_TOKEN_URL = `${MONDAY_PROTOCOL}://${MONDAY_OAUTH_SUBDOMAIN}${MONDAY_DOMAIN}/oauth2/token`;
 
