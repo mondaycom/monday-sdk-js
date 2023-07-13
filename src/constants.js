9d394265143f41ef6ccb7f131940fe6d9f52e721
Yoni Levin <parahall@gmail.com>
2023-07-13 15:54:21 +0300

    fix style

--- src/constants.js
+++ src/constants.js
@@ -2,13 +2,13 @@ const { isBrowser } = require("./helpers");
 
 const isNodeDevEnv = !isBrowser && process.env.NODE_ENV === "development";
 const getEnvOrDefault = (key, defaultVal) => {
-  return isNodeDevEnv && typeof process.env[key] !== 'undefined' ? process.env[key] : defaultVal;
+  return isNodeDevEnv && typeof process.env[key] !== "undefined" ? process.env[key] : defaultVal;
 };
 
-const MONDAY_PROTOCOL = getEnvOrDefault('MONDAY_COM_PROTOCOL', 'https');
-const MONDAY_DOMAIN = getEnvOrDefault('MONDAY_COM_DOMAIN', 'monday.com');
-const MONDAY_SUBDOMAIN_API = getEnvOrDefault('MONDAY_SUBDOMAIN_API', 'api.');
-const MONDAY_OAUTH_SUBDOMAIN = getEnvOrDefault(MONDAY_SUBDOMAIN_API,'auth.');
+const MONDAY_PROTOCOL = getEnvOrDefault("MONDAY_COM_PROTOCOL", "https");
+const MONDAY_DOMAIN = getEnvOrDefault("MONDAY_COM_DOMAIN", "monday.com");
+const MONDAY_SUBDOMAIN_API = getEnvOrDefault("MONDAY_SUBDOMAIN_API", "api.");
+const MONDAY_OAUTH_SUBDOMAIN = getEnvOrDefault(MONDAY_SUBDOMAIN_API, "auth.");
 
 const MONDAY_API_URL = `${MONDAY_PROTOCOL}://${MONDAY_SUBDOMAIN_API}${MONDAY_DOMAIN}/v2`;
 const MONDAY_OAUTH_URL = `${MONDAY_PROTOCOL}://${MONDAY_OAUTH_SUBDOMAIN}${MONDAY_DOMAIN}/oauth2/authorize`;
