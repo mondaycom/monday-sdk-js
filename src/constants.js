8122dace5bd921beae86069e49504b5df2de720b
Yoni Levin <parahall@gmail.com>
2023-07-20 14:40:11 +0300

    fix style

--- src/constants.js
+++ src/constants.js
@@ -5,7 +5,6 @@ const getEnvOrDefault = (key, defaultVal) => {
   return isNodeDevEnv && typeof process.env[key] !== "undefined" ? process.env[key] : defaultVal;
 };
 
-
 const MONDAY_PROTOCOL = () => getEnvOrDefault("MONDAY_COM_PROTOCOL", "https");
 const MONDAY_DOMAIN = () => getEnvOrDefault("MONDAY_COM_DOMAIN", "monday.com");
 const MONDAY_SUBDOMAIN_API = () => getEnvOrDefault("MONDAY_SUBDOMAIN_API", "api.");
@@ -13,7 +12,8 @@ const MONDAY_OAUTH_SUBDOMAIN = () => getEnvOrDefault(MONDAY_SUBDOMAIN_API(), "au
 
 const MONDAY_API_URL = () => `${MONDAY_PROTOCOL()}://${MONDAY_SUBDOMAIN_API()}${MONDAY_DOMAIN()}/v2`;
 const MONDAY_OAUTH_URL = () => `${MONDAY_PROTOCOL()}://${MONDAY_OAUTH_SUBDOMAIN()}${MONDAY_DOMAIN()}/oauth2/authorize`;
-const MONDAY_OAUTH_TOKEN_URL = () => `${MONDAY_PROTOCOL()}://${MONDAY_OAUTH_SUBDOMAIN()}${MONDAY_DOMAIN()}/oauth2/token`;
+const MONDAY_OAUTH_TOKEN_URL = () =>
+  `${MONDAY_PROTOCOL()}://${MONDAY_OAUTH_SUBDOMAIN()}${MONDAY_DOMAIN()}/oauth2/token`;
 
 module.exports = {
   get MONDAY_DOMAIN() {
