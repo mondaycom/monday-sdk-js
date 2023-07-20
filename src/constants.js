5fe537add1125487f94653b8e051fa82a01596a4
Yoni Levin <parahall@gmail.com>
2023-07-20 16:03:27 +0300

    Reworking condition how we take env variable

--- src/constants.js
+++ src/constants.js
@@ -1,14 +1,11 @@
-const { isBrowser } = require("./helpers");
-
-const isNodeDevEnv = !isBrowser && process.env.NODE_ENV === "development";
 const getEnvOrDefault = (key, defaultVal) => {
-  return isNodeDevEnv && typeof process.env[key] !== "undefined" ? process.env[key] : defaultVal;
+  return process.env[key] !== "undefined" ? process.env[key] : defaultVal;
 };
 
 const MONDAY_PROTOCOL = () => getEnvOrDefault("MONDAY_COM_PROTOCOL", "https");
 const MONDAY_DOMAIN = () => getEnvOrDefault("MONDAY_COM_DOMAIN", "monday.com");
 const MONDAY_SUBDOMAIN_API = () => getEnvOrDefault("MONDAY_SUBDOMAIN_API", "api.");
-const MONDAY_OAUTH_SUBDOMAIN = () => getEnvOrDefault(MONDAY_SUBDOMAIN_API(), "auth.");
+const MONDAY_OAUTH_SUBDOMAIN = () => getEnvOrDefault("MONDAY_OAUTH_SUBDOMAIN", "auth.");
 
 const MONDAY_API_URL = () => `${MONDAY_PROTOCOL()}://${MONDAY_SUBDOMAIN_API()}${MONDAY_DOMAIN()}/v2`;
 const MONDAY_OAUTH_URL = () => `${MONDAY_PROTOCOL()}://${MONDAY_OAUTH_SUBDOMAIN()}${MONDAY_DOMAIN()}/oauth2/authorize`;
