6a3906dedc673d86ea2927d3ee0002b2c9be8875
Yonatan Levin <parahall@gmail.com>
2023-07-20 14:41:27 +0300

    Make URL for monday dynamic (#109)

--- src/constants.js
+++ src/constants.js
@@ -5,19 +5,30 @@ const getEnvOrDefault = (key, defaultVal) => {
   return isNodeDevEnv && typeof process.env[key] !== "undefined" ? process.env[key] : defaultVal;
 };
 
-const MONDAY_PROTOCOL = getEnvOrDefault("MONDAY_COM_PROTOCOL", "https");
-const MONDAY_DOMAIN = getEnvOrDefault("MONDAY_COM_DOMAIN", "monday.com");
-const MONDAY_SUBDOMAIN_API = getEnvOrDefault("MONDAY_SUBDOMAIN_API", "api.");
-const MONDAY_OAUTH_SUBDOMAIN = getEnvOrDefault(MONDAY_SUBDOMAIN_API, "auth.");
+const MONDAY_PROTOCOL = () => getEnvOrDefault("MONDAY_COM_PROTOCOL", "https");
+const MONDAY_DOMAIN = () => getEnvOrDefault("MONDAY_COM_DOMAIN", "monday.com");
+const MONDAY_SUBDOMAIN_API = () => getEnvOrDefault("MONDAY_SUBDOMAIN_API", "api.");
+const MONDAY_OAUTH_SUBDOMAIN = () => getEnvOrDefault(MONDAY_SUBDOMAIN_API(), "auth.");
 
-const MONDAY_API_URL = `${MONDAY_PROTOCOL}://${MONDAY_SUBDOMAIN_API}${MONDAY_DOMAIN}/v2`;
-const MONDAY_OAUTH_URL = `${MONDAY_PROTOCOL}://${MONDAY_OAUTH_SUBDOMAIN}${MONDAY_DOMAIN}/oauth2/authorize`;
-const MONDAY_OAUTH_TOKEN_URL = `${MONDAY_PROTOCOL}://${MONDAY_OAUTH_SUBDOMAIN}${MONDAY_DOMAIN}/oauth2/token`;
+const MONDAY_API_URL = () => `${MONDAY_PROTOCOL()}://${MONDAY_SUBDOMAIN_API()}${MONDAY_DOMAIN()}/v2`;
+const MONDAY_OAUTH_URL = () => `${MONDAY_PROTOCOL()}://${MONDAY_OAUTH_SUBDOMAIN()}${MONDAY_DOMAIN()}/oauth2/authorize`;
+const MONDAY_OAUTH_TOKEN_URL = () =>
+  `${MONDAY_PROTOCOL()}://${MONDAY_OAUTH_SUBDOMAIN()}${MONDAY_DOMAIN()}/oauth2/token`;
 
 module.exports = {
-  MONDAY_DOMAIN,
-  MONDAY_PROTOCOL,
-  MONDAY_API_URL,
-  MONDAY_OAUTH_URL,
-  MONDAY_OAUTH_TOKEN_URL
+  get MONDAY_DOMAIN() {
+    return MONDAY_DOMAIN();
+  },
+  get MONDAY_PROTOCOL() {
+    return MONDAY_PROTOCOL();
+  },
+  get MONDAY_API_URL() {
+    return MONDAY_API_URL();
+  },
+  get MONDAY_OAUTH_URL() {
+    return MONDAY_OAUTH_URL();
+  },
+  get MONDAY_OAUTH_TOKEN_URL() {
+    return MONDAY_OAUTH_TOKEN_URL();
+  }
 };
