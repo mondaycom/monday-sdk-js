85fa763a5035e80ca006eee4ffff70fb3f72a5fc
Adam Rutkowski <adamru@monday.com>
2023-08-11 14:25:38 +0200

    Don't use ES2020 features
    ESLint does not like them

--- src/helpers/monday-api-helpers.js
+++ src/helpers/monday-api-helpers.js
@@ -1,18 +1,19 @@
 const logWarnings = res => {
-  const warnings = res?.extensions?.warnings;
+  const warnings = res && res.extensions && res.extensions.warnings;
   if (!warnings || !Array.isArray(warnings)) return res;
 
   warnings.forEach(warning => {
-    if (!warning?.message) return;
+    if (!warning || !warning.message) return;
 
     try {
-      const locations = warning.locations?.map(loc => `line ${loc.line}, column ${loc.column}`).join("; ");
-      const path = warning.path?.join(" → ");
+      const locations =
+        warning.locations && warning.locations.map(loc => `line ${loc.line}, column ${loc.column}`).join("; ");
+      const path = warning.path && warning.path.join(" → ");
 
       let message = warning.message;
 
       // remove the dot at the end of the message
-      message = warning.message.replace(/\.$/, "");
+      message = message.replace(/\.$/, "");
       // start the message with lower case letter
       message = message.charAt(0).toLowerCase() + message.slice(1);
 
