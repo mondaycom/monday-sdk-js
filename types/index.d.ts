d7fb7d6984b76f0083f39ccc0fdce47d1c20c3f2
shay <shay.elkana@gmail.com>
2023-09-04 09:28:59 +0300

    feature: export interfaces of client sdk and server sdk

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,21 +1,8 @@
 // Original Definitions were contributed by: Josh Parnham <https://github.com/josh->
-import { ClientData } from './client-data.interface';
-import { ClientExecute } from './client-execute.interface';
-import { ClientApi, APIOptions } from './client-api.interface';
+import {MondayClientSdk} from "./client-sdk.interface";
+import {MondayServerSdk} from "./server-sdk.interface";
 
 export as namespace mondaySdk;
-
-type MondayClientSdk = ClientData & ClientExecute & ClientApi;
-
-interface MondayServerSdk {
-    setToken(token: string): void;
-
-    setApiVersion(version: string): void;
-
-    api(query: string, options?: APIOptions): Promise<any>;
-
-    oauthToken(code: string, clientId: string, clientSecret: string): Promise<any>;
-}
 
 declare function init(
     config?: Partial<{
