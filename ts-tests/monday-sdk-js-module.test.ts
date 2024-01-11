6a7d665dcdfb18c8b21ac440e11402f68e9cb734
rami-monday <rami@monday.com>
2024-01-11 11:44:25 +0200

    remove  fro ts-tests

--- ts-tests/monday-sdk-js-module.test.ts
+++ ts-tests/monday-sdk-js-module.test.ts
@@ -1,73 +1,70 @@
 import mondaySdk from "../types";
 const monday = mondaySdk();
 
-monday.api("test"); // $ExpectType Promise<{ data: object; }>
+monday.api("test");
 
-monday.setApiVersion("2023-10"); // $ExpectType void
+monday.setApiVersion("2023-10");
 mondaySdk({ apiVersion: "2023-10" });
-monday.api("test", { apiVersion: "2023-07" }); // $ExpectType Promise<{ data: object; }>
+monday.api("test", { apiVersion: "2023-07" });
 
-monday.setToken("test"); // $ExpectType void
+monday.setToken("test");
 
 monday.get("context", { appFeatureType: "AppFeatureBoardView" }).then(res => {
-  const { data } = res;
-  data.app.id; // $ExpectType number
-  data.permissions.approvedScopes; // $ExpectType string[]
-  data.theme; // $ExpectType string
-  data.boardId; // $ExpectType number
-  data.viewMode; // $ExpectType string
-}); // $ExpectType Promise<{ data: string; }>
+  const { data }: { data: { app: { id: number }; theme: string; boardId: number; viewMode: string } } = res;
+});
 
 monday.get<{ text: string; level: number }>("settings").then(res => {
-  const { data } = res;
-  data.text; // $ExpectType string
-  data.level; // $ExpectType number
-}); // $ExpectType Promise<{ data: object; }>
+  const { data }: { data: { text: string; level: number } } = res;
+});
 
 monday.get("itemIds").then(res => {
-  const { data } = res;
-  data[0]; // $ExpectType number
-}); // $ExpectType Promise<{ data: number[]; }>
+  const { data }: { data: number[] } = res;
+});
 
 monday.get("sessionToken").then(res => {
-  const { data } = res;
-  data; // $ExpectType string
-}); // $ExpectType Promise<{ data: string; }>
+  const { data }: { data: string } = res;
+});
 
-monday.set("settings", { text: "this is a test", number: 23 }); // $ExpectType Promise<any>
+monday.set("settings", { text: "this is a test", number: 23 });
 
-monday.listen("context", res => res); // $ExpectType void
+monday.listen(
+  "context",
+  res => {
+    const { data }: { data: { app: { id: number }; theme: string; itemId: number } } = res;
+  },
+  { appFeatureType: "AppFeatureItemView" }
+);
 
-monday.execute("openItemCard", { itemId: 123 }); // $ExpectType Promise<any>
-monday.execute("confirm", { message: "Hello" }); // $ExpectType Promise<{ data: { confirm: boolean; }; }>
-monday.execute("notice", { message: "Hello" }); // $ExpectType Promise<any>
-// $ExpectType Promise<any>
+monday.execute("openItemCard", { itemId: 123 });
+monday.execute("confirm", { message: "Hello" });
+monday.execute("notice", { message: "Hello" });
+
 monday.execute("openFilesDialog", {
   boardId: 12345,
   itemId: 23456,
   columnId: "files",
   assetId: 34567
 });
-// $ExpectType Promise<any>
+
 monday.execute("triggerFilesUpload", {
   boardId: 12345,
   itemId: 23456,
   columnId: "files"
 });
-monday.execute("openAppFeatureModal", { urlPath: "/path", urlParams: {}, width: "100px", height: "100px" }); // $ExpectType Promise<{ data: any; }>
-monday.execute("closeAppFeatureModal"); // $ExpectType Promise<{ data: any; }>
-monday.execute("valueCreatedForUser"); // $ExpectType Promise<any}>
-// $ExpectType Promise<any>
+monday.execute("openAppFeatureModal", { urlPath: "/path", urlParams: {}, width: "100px", height: "100px" });
+monday.execute("closeAppFeatureModal");
+monday.execute("valueCreatedForUser");
+
 monday.execute("addDocBlock", {
   type: "normal text",
   content: { deltaFormat: [{ insert: "test" }] }
 });
-// $ExpectType Promise<any>
+
 monday.execute("updateDocBlock", {
   id: "1234-1234-23434dsf",
   content: { deltaFormat: [{ insert: "test" }] }
 });
-// $ExpectType Promise<any>
+
 monday.execute("addMultiBlocks", {
   afterBlockId: "1234-1234-23434dsf",
   blocks: [
@@ -77,49 +74,41 @@ monday.execute("addMultiBlocks", {
     }
   ]
 });
-monday.execute("closeDocModal"); // $ExpectType Promise<any>
+monday.execute("closeDocModal");
 
 monday.oauth({ clientId: "clientId" });
 
 monday.storage.instance.getItem("test").then(res => {
-  const { data } = res;
-  data.success; // $ExpectType boolean
-  data.value; // $ExpectType any
-}); // $ExpectType Promise<GetResponse>
+  const { data }: { data: { error?: string; success: boolean }; errorMessage?: string } = res;
+});
 
 monday.storage.instance.setItem("test", "123").then(res => {
-  const { data } = res;
-  data.success; // $ExpectType boolean
-}); // $ExpectType Promise<SetResponse>
+  const { data }: { data: { error?: string; success: boolean }; errorMessage?: string } = res;
+});
 
 monday.storage.instance.deleteItem("test").then(res => {
-  const { data } = res;
-  data.success; // $ExpectType boolean
-}); // $ExpectType Promise<DeleteResponse>
+  const { data }: { data: { error?: string; success: boolean }; errorMessage?: string } = res;
+});
 
 monday.storage.getItem("test").then(res => {
-  const { data } = res;
-  data.success; // $ExpectType boolean
-  data.value; // $ExpectType any
-}); // $ExpectType Promise<GetResponse>
+  const { data }: { data: { error?: string; success: boolean }; errorMessage?: string } = res;
+});
 
 monday.storage.setItem("test", "123").then(res => {
-  const { data } = res;
-  data.success; // $ExpectType boolean
-}); // $ExpectType Promise<SetResponse>
+  const { data }: { data: { error?: string; success: boolean }; errorMessage?: string } = res;
+});
 
 monday.storage.deleteItem("test").then(res => {
-  const { data } = res;
-  data.success; // $ExpectType boolean
-}); // $ExpectType Promise<DeleteResponse>
+  const { data }: { data: { error?: string; success: boolean }; errorMessage?: string } = res;
+});
 
 const mondayServer = mondaySdk({ token: "123", apiVersion: "2023-10" });
 
-mondayServer.setToken("123"); // $ExpectType void
-mondayServer.setApiVersion("2023-10"); // $ExpectType void
-mondayServer.api("test"); // $ExpectType Promise<any>
-mondayServer.api("test", { token: "test" }); // $ExpectType Promise<any>
-mondayServer.api("test", { variables: { variable1: "test" } }); // $ExpectType Promise<any>
-mondayServer.api("test", { token: "test", variables: { variable1: "test" } }); // $ExpectType Promise<any>
-mondayServer.api("test", { token: "test", apiVersion: "2023-07" }); // $ExpectType Promise<any>
-mondayServer.oauthToken("test", "test", "test"); // $ExpectType Promise<any>
+mondayServer.setToken("123");
+mondayServer.setApiVersion("2023-10");
+mondayServer.api("test");
+mondayServer.api("test", { token: "test" });
+mondayServer.api("test", { variables: { variable1: "test" } });
+mondayServer.api("test", { token: "test", variables: { variable1: "test" } });
+mondayServer.api("test", { token: "test", apiVersion: "2023-07" });
+mondayServer.oauthToken("test", "test", "test");
