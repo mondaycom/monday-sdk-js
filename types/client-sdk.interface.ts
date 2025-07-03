165f59a93a25eb324540c6ce3a40c9c11adb275b
Moshik Eilon <moshik.eilon@gmail.com>
2025-07-03 16:00:54 +0300

    feat: add typed board and item APIs

--- types/client-sdk.interface.ts
+++ types/client-sdk.interface.ts
@@ -1,5 +1,9 @@
 import { ClientData } from './client-data.interface';
 import { ClientExecute } from './client-execute.interface';
 import { ClientApi } from './client-api.interface';
+import { BoardsApi, ItemsApi } from '../src/boards-sdk/types';
 
-export type MondayClientSdk = ClientData & ClientExecute & ClientApi;
+export type MondayClientSdk = ClientData & ClientExecute & ClientApi & {
+  boards: BoardsApi;
+  items: ItemsApi;
+};
